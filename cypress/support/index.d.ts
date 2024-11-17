785b43d4d9e3fa4a2bf2dc2d282d31a0a8257d97
Fazil M <dumpdusty@gmail.com>
2024-11-16 22:46:12 -0800

    add custom login command

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,14 @@
+// declaring types not required in newer versions of IDE
+// code commented below will produce an error inside of support/commands.js but custom command will work
+// to get rid of the error just uncomment code below
+
+// declare namespace Cypress {
+//     interface Chainable<Subject> {
+//       /**
+//        * Log into app
+//        * @example
+//        * cy.login('email', 'password')
+//        */
+//       login(email: string, password: string): void;
+//     }
+//   }
