045fa1a06dbf6fc5efdc3fa3a8cf2916ce25e579
Fazil M <dumpdusty@gmail.com>
2024-12-20 16:54:52 -0800

    add profile spec

---
+++ cypress/e2e/ui/profile.cy.js
@@ -0,0 +1,7 @@
+import ProfilePage from "../../pages/intPages/infoPages/profilePage";
+
+describe(`USER PROFILE`, () => {
+    it('open profile page', () => {
+        ProfilePage.open();
+    });
+});
