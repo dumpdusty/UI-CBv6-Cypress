c4a8afeb9651662b14d63a895f1152835755547d
Fazil M <dumpdusty@gmail.com>
2024-12-20 16:15:23 -0800

    cleanup sandbox.cy.js

--- cypress/e2e/sandbox.cy.js
+++
@@ -1,9 +0,0 @@
-import ProfilePage from '../pages/intPages/infoPages/profilePage';
-import AuthPage from "../pages/extPages/authPage";
-import CompanyPage from "../pages/intPages/infoPages/companyPage";
-
-describe('API Login Test', () => {
-    it('should log the user ID after login', () => {
-      CompanyPage.open()
-    });
-});
