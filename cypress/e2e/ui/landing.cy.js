d92cbe8ecf33460b5b12a5993da1c5ab1716d553
Fazil M <dumpdusty@gmail.com>
2024-12-20 16:25:23 -0800

    add landing page tests

--- cypress/e2e/ui/landing.cy.js
+++ cypress/e2e/ui/landing.cy.js
@@ -6,7 +6,16 @@ describe('LANDING PAGE', () => {
         LandingPage.open()
         LandingPage.logo.should(`be.visible`).and(`have.text`, LABEL.APP_NAME)
     });
-    it('verify header elements', () => {
 
+    it('verify header elements', () => {
+        LandingPage.open()
+        LandingPage.verifyHeaderElements(`.header-logo`, LABEL.APP_NAME)
+        LandingPage.verifyHeaderElements(`.card-title`, LABEL.REGISTRATION_PAGE.FORM_NAME)
+        LandingPage.verifyHeaderElements(`.card-text`, LABEL.REGISTRATION_PAGE.MESSAGE)
+    });
+
+    it.only('verify page fields', () => {
+        LandingPage.open()
+        LandingPage.verifyPageFields()
     });
 });
