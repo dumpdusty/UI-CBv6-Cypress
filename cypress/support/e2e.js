604f9a0745ca9e1c8e40651ba8ed0220ef02f531
Fazil M <dumpdusty@gmail.com>
2024-12-26 16:42:00 -0800

    add uncaught:exception exclusion

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,10 +16,12 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
-Cypress.on('uncaught:exception', (err, runnable) => {
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+
+Cypress.on('uncaught:exception', () => {
     // returning false here prevents Cypress from
     // failing the test
     return false
 })
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
